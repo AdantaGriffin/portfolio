76a8ebaee7620c54008d5124bc7f58cb378f839a
Adanta Griffin <griffinadanta90@gmail.com>
2024-10-03 22:53:13 -0400

    updated img alt

--- src/components/about/me.js
+++ src/components/about/me.js
@@ -9,7 +9,7 @@ function Me(){
             <p className='me-text'>
                Is it fair to say that everyone's first love is food? Well mine was and so was it my first passion as well. Since a young age I would always love to be involved with my mothers cooking and when I was allowed to cook for myself, the passion only grew.
             </p>
-            <img src='/images/IMG_6411.PNG' height='1200px'width='900px'/>
+            <img src='/images/IMG_6411.PNG' height='1200px'width='900px' alt=''/>
          </div>
          <div className='me-gallery'>
             <img className='me-pic' src='' height='150px' width='300px' alt=''/>
